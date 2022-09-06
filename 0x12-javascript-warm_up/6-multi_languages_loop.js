b4fd08a1ad0e8878f7f749156ab8afca23cb77d5
Roymaniac <amolojoseph2020@gmail.com>
2022-09-06 02:48:48 +0100

    javascript task

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const words = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const s = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < 3; i++) {
-  console.log(words[i]);
+  console.log(s[i]);
 }
