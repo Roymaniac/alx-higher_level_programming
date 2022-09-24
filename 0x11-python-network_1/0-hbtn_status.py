76bf079f5f459c899404c47d838ef963e445f2c2
Roymaniac <amolojoseph2020@gmail.com>
2022-09-24 01:07:02 +0100

    fixes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 
 import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
     req = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
     print(req.format(type(html), html, html.decode('utf-8')))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,6 +4,6 @@
 import requests
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
-    req = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-    print(req.format(type(r.text), r.text))
+    response = requests.get('https://intranet.hbtn.io/status')
+    req = "Body response:\n\t- type: {}\n\t- content: {}"
+    print(req.format(type(response.text), response.text))
